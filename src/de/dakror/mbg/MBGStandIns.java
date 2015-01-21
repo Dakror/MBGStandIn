b1de2c0e550ddfd3e9061a232774762e89f7653e
Dakror <mail@dakror.de>
2015-01-21 17:21:29 +0100

    settings as an activity

--- src/de/dakror/mbg/MBGStandIns.java
+++ src/de/dakror/mbg/MBGStandIns.java
@@ -1,9 +1,9 @@
 package de.dakror.mbg;
 
 import android.app.Activity;
-import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.os.Bundle;
+import android.preference.PreferenceFragment;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -12,11 +12,35 @@ import android.view.MenuItem;
  * @author Maximilian Stark | Dakror
  */
 public class MBGStandIns extends Activity {
+	/**
+	 * @author Maximilian Stark | Dakror
+	 */
+	public static class SettingsFragment extends PreferenceFragment {
+		@Override
+		public void onCreate(Bundle savedInstanceState) {
+			super.onCreate(savedInstanceState);
+			addPreferencesFromResource(R.xml.settings);
+		}
+	}
+	
+	/**
+	 * @author Maximilian Stark | Dakror
+	 */
+	public static class SettingsActivity extends Activity {
+		
+		@Override
+		public void onCreate(Bundle savedInstanceState) {
+			super.onCreate(savedInstanceState);
+			setContentView(R.layout.acitivity_settings);
+		}
+	}
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_mbgstandin);
-		startService(new Intent(this, NotificationService.class));
+		Intent service = new Intent(this, NotificationService.class);
+		startService(service);
 	}
 	
 	@Override
@@ -30,8 +54,8 @@ public class MBGStandIns extends Activity {
 	public boolean onOptionsItemSelected(MenuItem item) {
 		switch (item.getItemId()) {
 			case R.id.settings:
-				getFragmentManager().beginTransaction().add(android.R.id.content, new SettingsFragment()).setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN).setBreadCrumbTitle(R.string.settings).addToBackStack(null).commit();
-				
+				Intent intent = new Intent(this, SettingsActivity.class);
+				startActivity(intent);
 			default:
 				return super.onOptionsItemSelected(item);
 		}
