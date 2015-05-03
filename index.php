3c5d8ab7714393e208925f2d150ffb2997aeaa6f
Dakror <mail@dakror.de>
2015-05-03 14:53:49 +0200

    Removed test stuff

--- index.php
+++ index.php
@@ -19,27 +19,4 @@ set_time_limit(0);
 
 $parser = new StandInParser();
 
-// 37a08ed30093a133b1bb4ae0b8f3601f
-
-$parser->update("37a08ed30093a133b1bb4ae0b8f3601f");
-
-#echo nl2br($parser->getText( /*$parser->fetchFile(true, md5("einstein"))*/file_get_contents("morgen.pdf")));
-
-#$table = $parser->parseAllPages(file_get_contents("morgen.pdf"));
-
-#$parser->store($table);
-
-#if(__DEBUG__) {
-#	echo "<br><br>>>>>>>>>>>>>>>>>>>>>>><br><br><br><pre>";
-#	echo $table;
-#print_r($table->getStandIns()[2]);
-#
-#echo "<br><br>>>>>>>>>>>>>>>>>>>>>>><br><br><br>";
-#
-#print_r($table->getStandIns()[1]);
-#
-#echo "<br><br>>>>>>>>>>>>>>>>>>>>>>><br><br><br>";
-#
-#print_r($table->getStandIns()[19]);
-#}
 ?>
